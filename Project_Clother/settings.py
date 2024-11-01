08582b7ba108d512cee21354c2b59106fbcf6547
Hồ Thanh Tỷ <92771307+THANHTY122@users.noreply.github.com>
2024-11-02 01:30:22 +0700

    Update settings.py

--- Project_Clother/settings.py
+++ Project_Clother/settings.py
@@ -120,7 +120,7 @@ ROOT_URLCONF = 'Project_Clother.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -132,6 +132,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'Project_Clother.wsgi.application'
 
