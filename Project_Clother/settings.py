e3395bf445a0529c53811c1c155e60deb525e9b5
Hồ Thanh Tỷ <92771307+THANHTY122@users.noreply.github.com>
2024-11-21 21:03:17 +0700

    Update settings.py

--- Project_Clother/settings.py
+++ Project_Clother/settings.py
@@ -89,13 +89,15 @@ SOCIALACCOUNT_LOGIN_ON_GET = True
 #    'shop-quan-ao-git-doanopenshift-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com'
 #]
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    'shop-quan-ao-git-nvthanhty-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com',
-    'shop-quan-ao-git-doanopenshift-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com',
-    'shop-clother-git-nvthanhty-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com'
-]
+# ALLOWED_HOSTS = [
+#     'localhost',
+#     '127.0.0.1',
+#     'shop-quan-ao-git-nvthanhty-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com',
+#     'shop-quan-ao-git-doanopenshift-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com',
+#     'shop-clother-git-nvthanhty-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com'
+# ]
+
+ALLOWED_HOSTS = ['*']
 
 
 
