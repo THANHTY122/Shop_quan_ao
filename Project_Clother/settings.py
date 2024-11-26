1c3f27e5e72267409908386de7d65a442727bdd7
Hồ Thanh Tỷ <92771307+THANHTY122@users.noreply.github.com>
2024-11-26 16:41:52 +0700

    Update settings.py

--- Project_Clother/settings.py
+++ Project_Clother/settings.py
@@ -113,6 +113,9 @@ ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True
 
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -221,7 +224,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 MEDIA_URL ='/media/'
 MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
